48be6e72c926abd426dbe3612c2b8ad8540e4248
Walid ichou <155623558+draken218@users.noreply.github.com>
2025-12-19 13:28:47 +0100

    Add files via upload

---
+++ gestion-biblio/src/main/java/util/CSVUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+public class CSVUtil {
+
+    public static List<Livre> importLivres(List<String> lignes);
+}
+
+
+
+    
---
+++ gestion-biblio/src/main/java/util/PasswordUtil.java
@@ -0,0 +1,10 @@
+package util;
+
+public class PasswordUtil {
+
+    public static String hashPassword(String plain);
+
+    public static boolean checkPassword(String plain, String hashed);
+    
+
+}
